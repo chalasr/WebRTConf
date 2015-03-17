34fa24720edbc75abf291ea48071fbbdd5579d82
chalasr <robin.chalas@epitech.eu>
2015-03-17 20:53:20 +0100

    upd

--- README.md
+++
@@ -1,4 +0,0 @@
-Multi-user conference app made with AngularJS, Express, SocketIO & WebRTC
-================
-
-[Demo](http://chalasr-webrtc-5555.herokuapp.com/).
--- index.js
+++
@@ -1,6 +0,0 @@
-var config = require('./config/config.json'),
-    server = require('./lib/server');
-
-config.PORT = process.env.PORT || config.PORT;
-
-server.run(config);
